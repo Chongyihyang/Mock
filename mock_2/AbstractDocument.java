d51bd58a5b15e40e32b4442ddebac77671c137ad
Yih Yang <chongyihyang713@gmail.com>
2026-09-21 19:39:14 +0800

    correction1 for mock_2

--- mock_2/AbstractDocument.java
+++ mock_2/AbstractDocument.java
@@ -1,12 +1,14 @@
 public abstract class AbstractDocument implements Printable {
 
-  private String name;
-  private int pages;
+  private final String name;
+  private final int pages;
 
   public AbstractDocument(String name, int pages) throws IllegalArgumentException {
-    if (name == null || name.equals("")) {
+    if (name == null || name.trim().equals("")) {
       throw new IllegalArgumentException();
-    }
+    } else if (pages <= 0) {
+      throw new IllegalArgumentException();
+    } 
     this.name = name;
     this.pages = pages;
   }
